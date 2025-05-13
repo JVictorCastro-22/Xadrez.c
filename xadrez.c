82c1d4f63398871e78f09f2e211a7f83255c1ebf
JVictorCastro-22 <joao.souzacastro525@gmail.com>
2025-05-13 23:58:45 +0000

    criando a peça cavalo

--- xadrez.c
+++ xadrez.c
@@ -3,7 +3,7 @@
 int main() {
 
     // Movimentação da Torre usando 'for'
-    printf("Movimentação da Torre:\n");
+    printf("\nMovimentação da Torre:\n");
     for (int i = 1; i <= 5; i++) {
         printf("Direita: %d casa(s)\n", i);
     }
@@ -23,6 +23,18 @@ int main() {
         printf("Esquerda: %d casa(s)\n", k);
         k++;
     } while (k <= 8);
+
+    // Movimentação do Cavalo usando loops aninhados
+    printf("\nMovimentação do Cavalo:\n");
+    int movimentocavalo = 1; // Definir um número maior se quiser mais movimentos
+    
+    int passo = 1; // Criando um contador para o número de movimentos
+    while (movimentocavalo--) {
+        for (int i = 0; i < 2; i++) {
+            printf("Cima: %d casa(s)\n", passo++);
+        }
+        printf("Direita: %d casa(s)\n", passo++);
+    }
 
     return 0;
 }
